beca0ffd9e693bdd6ada2821295bfa1bded19ca2
Amimaro <amir.zahlan@gmail.com>
2018-05-22 11:43:55 -0300

    Update project :hammer:

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  router,
+  store,
   render: h => h(App)
-})
+}).$mount("#app");
