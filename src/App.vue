44aed09905b12d1078cb5483fb7ac5c314c9b033
Amimaro <amir.zahlan@gmail.com>
2018-05-22 17:40:07 -0300

    Set background color :art:

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,12 @@
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: gray;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is an home page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
