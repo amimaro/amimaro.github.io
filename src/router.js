aeb226c802d45babe3f4427666454ba35c11b4a9
Amimaro <amir.zahlan@gmail.com>
2018-05-23 15:57:04 -0300

    Add title to routes :hammer:

--- src/router.js
+++ src/router.js
@@ -1,33 +1,44 @@
 import Vue from "vue";
-import Router from "vue-router";
+import VueRouter from "vue-router";
 import Home from "./views/Home.vue";
 import Projects from "./views/Projects.vue";
 import Certificates from "./views/Certificates.vue";
 import Books from "./views/Books.vue";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+const Router = new VueRouter({
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      meta: { title: 'amir zahlan | fullstack developer' }
     },
     {
       path: "/projects",
       name: "projects",
-      component: Projects
+      component: Projects,
+      meta: { title: 'projects | amir zahlan | fullstack developer' }
     },
     {
       path: "/certificates",
       name: "certificates",
-      component: Certificates
+      component: Certificates,
+      meta: { title: 'certificates | amir zahlan | fullstack developer' }
     },
     {
       path: "/books",
       name: "books",
-      component: Books
+      component: Books,
+      meta: { title: 'books | amir zahlan | fullstack developer' }
     }
   ]
 });
+
+Router.beforeEach((to, from, next) => {
+    document.title = to.meta.title
+    next()
+});
+
+export default Router;
