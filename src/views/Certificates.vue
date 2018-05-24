ff79b06c3a500be77051dc50787a44573f0da78c
Amimaro <amir.zahlan@gmail.com>
2018-05-24 10:13:58 -0300

    Load certificates :art:

--- src/views/Certificates.vue
+++ src/views/Certificates.vue
@@ -3,3 +3,15 @@
     <h1>This is a certificates page</h1>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Certificates",
+  props: {},
+  computed: {
+    ...mapGetters("certificate", ["images"])
+  }
+};
+</script>
