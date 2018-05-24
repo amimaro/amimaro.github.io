0a5fd0b8f2785a3381d4396a8b1acd74c715176b
Amimaro <amir.zahlan@gmail.com>
2018-05-24 12:14:13 -0300

    Componentize options :art:

---
+++ src/components/Options.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="options list">
+    <div class="list-item">
+      <a href="https://www.linkedin.com/in/amir-zahlan-2143806b/" target="_blank">
+        <icon name="brands/linkedin" scale="2"></icon>
+      </a>
+    </div>
+    <div class="list-item">
+      <a href="https://github.com/amimaro" target="_blank">
+        <icon name="brands/github" scale="2"></icon>
+      </a>
+    </div>
+    <div class="list-item">
+      <a href="#" v-on:click="$parent.$refs.contact.toggleEmail()">
+        <icon name="envelope" scale="2"></icon>
+      </a>
+    </div>
+    <div class="list-item">
+      <router-link to="/certificates">
+        <icon name="certificate" scale="2"></icon>
+      </router-link>
+    </div>
+    <div class="list-item">
+      <router-link to="/projects">
+        <icon name="folder-open" scale="2"></icon>
+      </router-link>
+    </div>
+    <div class="list-item">
+      <router-link to="/books">
+        <icon name="book" scale="2"></icon>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "options"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,7 @@
 <template>
   <div id="home">
       <Name/>
-      <div class="list">
-        <div class="list-item">
-          <a href="https://www.linkedin.com/in/amir-zahlan-2143806b/" target="_blank">
-            <icon name="brands/linkedin" scale="2"></icon>
-          </a>
-        </div>
-        <div class="list-item">
-          <a href="https://github.com/amimaro" target="_blank">
-            <icon name="brands/github" scale="2"></icon>
-          </a>
-        </div>
-        <div class="list-item">
-          <a href="#" v-on:click="$refs.contact.toggleEmail()">
-            <icon name="envelope" scale="2"></icon>
-          </a>
-        </div>
-        <div class="list-item">
-          <router-link to="/certificates">
-            <icon name="certificate" scale="2"></icon>
-          </router-link>
-        </div>
-        <div class="list-item">
-          <router-link to="/projects">
-            <icon name="folder-open" scale="2"></icon>
-          </router-link>
-        </div>
-        <div class="list-item">
-          <router-link to="/books">
-            <icon name="book" scale="2"></icon>
-          </router-link>
-        </div>
-      </div>
+      <Options/>
       <Contact ref="contact"/>
   </div>
 </template>
@@ -40,12 +9,14 @@
 <script>
 import Name from "@/components/Name";
 import Contact from "@/components/Contact";
+import Options from "@/components/Options";
 
 export default {
   name: "home",
   components: {
     Name,
-    Contact
+    Contact,
+    Options
   }
 };
 </script>
