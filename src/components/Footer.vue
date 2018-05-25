4ec6fa447562637bb179b9138eac75c0c797eae7
Amimaro <amir.zahlan@gmail.com>
2018-05-25 12:18:18 -0300

    Create Footer :art:

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+  data() {
+    return {
+      msg: "2018 amimaro"
+    };
+  }
+};
+</script>
