915e493cb26d2ee725b0c23c17cd7d25e0179721
amimaro <amir.zahlan@gmail.com>
2018-05-26 16:57:08 -0300

    Update certificates columns layout :art:

--- src/views/Certificates.vue
+++ src/views/Certificates.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="certificates" class="has-text-centered">
     <h1 class="title">Certificates</h1>
-    <div class="columns">
-      <div class="column" v-for="(certificate, index) in certificates"
+    <div class="columns  is-multiline is-mobile">
+      <div class="column is-half" v-for="(certificate, index) in certificates"
       v-bind:key="index">
         <img :src="certificate" alt="certificate" class="list-item">
       </div>
