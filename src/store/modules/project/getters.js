ab642559c623e128099476fcf19ee3094f03516e
amimaro <amir.zahlan@gmail.com>
2018-05-27 08:00:03 -0300

    Filter projects :sparkles:

--- src/store/modules/project/getters.js
+++ src/store/modules/project/getters.js
@@ -1,5 +1,20 @@
 export default {
   projects: state => {
-    return state.list;
+    const repos = [
+      "Provas-POSCOMP",
+      "generator-angular-api",
+      "dungeon-crawler",
+      "game-of-life",
+      "strobe",
+      "is-stargazer"
+    ];
+    return state.list
+      .filter(project => {
+        if (repos.includes(project.name)) return true;
+        return false;
+      })
+      .sort((a, b) => {
+        return a.stargazers_count < b.stargazers_count;
+      });
   }
 };
