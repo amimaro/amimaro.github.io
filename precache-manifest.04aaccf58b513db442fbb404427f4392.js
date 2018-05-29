b7b15b0ba679fc27b9e480f029f5114b9a915a7b
Amimaro <amir.zahlan@gmail.com>
2018-05-29 16:57:24 -0300

    Deploy to ghp :rocket:

---
+++ precache-manifest.04aaccf58b513db442fbb404427f4392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec28636ebfd052cbf57",
+    "url": "/js/vendors~app.bec28636.js"
+  },
+  {
+    "revision": "6675e8e99ebb75861d58",
+    "url": "/js/app.6675e8e9.js"
+  },
+  {
+    "revision": "2ff6ef479d806a4be2e00d4e33d4d372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec28636ebfd052cbf57",
+    "url": "/css/vendors~app.0.5097d3cf.css"
+  },
+  {
+    "revision": "6675e8e99ebb75861d58",
+    "url": "/css/app.d0120879.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9ea3e71d7c004417539956f4302e454.js"
+  "/precache-manifest.04aaccf58b513db442fbb404427f4392.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "amimaro.github.io"});
