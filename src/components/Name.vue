cea1a440b8d99ac83f971b909d70f425c709d255
Amimaro <amir.zahlan@gmail.com>
2018-05-30 09:16:43 -0300

    :sparkles: Update name with typed.js

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="name">
-    <h1 class="title">
-      {{names[nameIndex]}}
-    </h1>
+    <h1 class="title"></h1>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import Typed from "typed.js";
 
 export default {
   name: "name",
@@ -21,11 +20,14 @@ export default {
   },
   methods: {
     updateName: function() {
-      this.name = this.names;
-      setInterval(() => {
-        this.nameIndex++;
-        if (this.nameIndex >= this.name.length) this.nameIndex = 0;
-      }, 1000);
+      const options = {
+        strings: this.names,
+        typeSpeed: 200,
+        loop: true,
+        cursorChar: "_",
+        backSpeed: 50
+      };
+      new Typed(".title", options);
     }
   },
   computed: {
@@ -35,6 +37,12 @@ export default {
 </script>
 
 <style lang="scss">
+.title {
+  display: inline;
+}
+.typed-cursor {
+  font-size: 2rem;
+}
 #name {
   padding-top: 2vw;
   padding-bottom: 2vw;
