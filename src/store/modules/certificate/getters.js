79716b6984e025b05a24485196aea9f496cec140
amimaro <amir.zahlan@gmail.com>
2019-02-08 11:19:27 -0300

    :hammer: Filter certificate

--- src/store/modules/certificate/getters.js
+++ src/store/modules/certificate/getters.js
@@ -3,6 +3,7 @@ export default {
     return state.list
       .filter(element => {
         if (element.name.indexOf("README") >= 0) return false;
+        if (element.name.indexOf("20180605-google-analytics") >= 0) return false;
         return true;
       })
       .map(element => {
